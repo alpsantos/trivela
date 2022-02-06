8c55e2467f48ad9cf2ceb7a1cf123d6d1090b6a6
Alessandro Santos <alessandro.santos@nubank.com.br>
2022-02-06 14:12:56 -0300

    clean worker

--- src/trivela/components/worker.clj
+++ src/trivela/components/worker.clj
@@ -1,18 +1,14 @@
 (ns trivela.components.worker
-  (:require [com.stuartsierra.component :as component]
-            [trivela.protocols.worker :as protocols.worker]))
-
+  (:require [com.stuartsierra.component :as component]))
 
 (defn task-fn [this]
          (println "Executing task-fn")
          (Thread/sleep 300))
 
-(defn make-task2 [{:keys [flag] :as this}]
+(defn make-task [{:keys [flag] :as this}]
   (future
     (while @flag        ;; ...try/catch
       (task-fn this))))
-
-
 
 (defrecord Worker
   [options flag task]
@@ -29,22 +25,12 @@
   (stop [this]
     (reset! flag false)
     (while (not (realized? task))
-      ;(log/info "Waiting for the task to complete")
       (println "Waiting for the task to complete")
       (Thread/sleep 300))
-    (assoc this :flag nil :task nil))
+    (assoc this :flag nil :task nil)))
 
-  protocols.worker/IWorker
-  (make-task [this]
-    (future
-      (while @flag        ;; ...try/catch
-        (task-fn this))))
-  )
-
-;(def make-worker (map->Worker {:task2 make-task2}))
-(def make-worker (map->Worker {:options {:task-to-run make-task2} }))
+(def make-worker (map->Worker {:options {:task-to-run make-task} }))
 
 (def started (component/start make-worker))
 (type started)
-
 ;(component/stop started)
